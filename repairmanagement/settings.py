8b3ead16c872641c451c1a4064900560f4ecb5d6
Sadlybb <saeedbabaee70@gmail.com>
2022-04-07 21:03:56 +0430

    Admin panel started.

--- repairmanagement/settings.py
+++ repairmanagement/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     "django.contrib.staticfiles",
     'repairs',
+    'rest_framework',
 
 ]
 
--- repairs/admin.py
+++ repairs/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from django.contrib.auth.models import User
+
+from . import models
+
 # Register your models here.
+
+
+@admin.register(models.Customer)
+class Customer(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'phone1']
+    list_per_page = 10
+
+
+@admin.register(models.Device)
+class Device(admin.ModelAdmin):
+    list_display = ['brand', 'model', 'color']
+
+
+@admin.register(models.RepairOrder)
+class RepairOrder(admin.ModelAdmin):
+    list_display = ['placed_at', 'customer', 'model']
